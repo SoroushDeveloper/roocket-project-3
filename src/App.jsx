8d54f5afb33050303f926caee1c59a0161aba859
SoroushDev <mr.soroosh.qr@gmail.com>
2024-04-07 23:56:59 +0330

    New Packages: TailwindCSS, ReactRouterDOM, New Route: Home, New Components: Sidebar, SidebarButton, H1,

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from "./components/partial/sidebar.jsx";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Home from "./routes/home.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>,
+    },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Sidebar/>
+            <div className="p-4 sm:ml-64">
+                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                    <RouterProvider router={router}/>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
---
+++ src/routes/home.jsx
@@ -0,0 +1,7 @@
+import H1 from "../components/headings/h1.jsx";
+
+export default function Home() {
+    return (
+        <H1 text="Welcome Admin"/>
+    )
+}
